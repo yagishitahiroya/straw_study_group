84c5124b4561e07e15ede545c06c2c1ae8adeeec
柳下博哉 <yagishitahiroya@yagishitahiroshinarinoMacBook-Pro.local>
2020-04-21 13:07:49 +0900

    追加/編集/削除機能の追加

--- app/Controller/ThreadsController.php
+++ app/Controller/ThreadsController.php
@@ -12,9 +12,51 @@ class ThreadsController extends AppController {
             $this->Thread->create();
             if ($this->Thread->save($this->request->data)) {
                 $this->Flash->success(__('スレッドが保存されました'));
-                return $this->redirect(['action' => 'message']);
+                return $this->redirect(['action' => 'threads']);
             }
             $this->Flash->error(__('スレッドを追加できませんでした'));
         }
     }
+
+    public function edit($id = null) {
+        if (!$id) {
+            throw new NotFoundException(__('無効なスレッドです'));
+        }
+    
+        $thread = $this->Thread->findById($id);
+        if (!$thread) {
+            throw new NotFoundException(__('無効なスレッドです'));
+        }
+    
+        if ($this->request->is(array('post', 'put'))) {
+            $this->Thread->id = $id;
+            if ($this->Thread->save($this->request->data)) {
+                $this->Flash->success(__('編集に成功しました!'));
+                return $this->redirect(array('action' => 'threads'));
+            }
+            $this->Flash->error(__('編集に失敗しました'));
+        }
+    
+        if (!$this->request->data) {
+            $this->request->data = $thread;
+        }
+    }
+
+    public function delete($id = null) {
+        if ($this->request->is('get')) {
+            throw new MethodNotAllowedException();
+        }
+    
+        if ($this->Thread->delete($id)) {
+            $this->Flash->success(
+                __('スレッドの削除に成功しました。')
+            );
+        } else {
+            $this->Flash->error(
+                __('スレッドの削除に失敗しました。')
+            );
+        }
+    
+        return $this->redirect(array('action' => 'threads'));
+    }  
 }
