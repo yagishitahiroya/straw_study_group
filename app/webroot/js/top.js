edc8dcec8b9630de7eb41a385c5827b790b56f92
柳下博哉 <yagishitahiroya@yagishitahiroshinarinoMacBook-Pro.local>
2020-04-30 12:17:55 +0900

    js追加(使うか検討中)

--- app/webroot/js/top.js
+++ app/webroot/js/top.js
@@ -1,17 +1,17 @@
 $(function(){
-    var pagetop = $('#page_top');
+   var pagetop = $('#page_top');
     // ボタン非表示
-    pagetop.hide();
+   pagetop.hide();
     // 100px スクロールしたらボタン表示
-    $(window).scroll(function () {
-       if ($(this).scrollTop() > 100) {
+   $(window).scroll(function () {
+      if ($(this).scrollTop() > 100) {
             pagetop.fadeIn();
-       } else {
+      } else {
             pagetop.fadeOut();
-       }
-    });
-    pagetop.click(function () {
-       $('body, html').animate({ scrollTop: 0 }, 500);
-       return false;
-    });
-  });
+      }
+   });
+   pagetop.click(function () {
+      $('body, html').animate({ scrollTop: 0 }, 500);
+      return false;
+   });
+});
