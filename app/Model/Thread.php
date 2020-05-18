f197e18764d7e06a7570037fc6ff6bcce70b71e3
柳下博哉 <yagishitahiroya@yagishitahiroshinarinoMacBook-Pro.local>
2020-05-18 15:07:07 +0900

    ThreadLike追加

--- app/Model/Thread.php
+++ app/Model/Thread.php
@@ -1,11 +1,14 @@
 <?php
 
 
-class Thread extends AppModel {
+class Thread extends AppModel 
+{
     public $validate = ['title' => ['rule' => 'notBlank']];
 
     public $hasMany = ['Message' => ['className' => 'Message',
-                                    'dependent' => true]];
+                                    ],
+                        'ThreadLike' => ['className' => 'ThreadLike',
+                                        ]];
 
     public $belongsTo = 'User';
 }
